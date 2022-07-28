381747ce7cae75e32c6813b28eab3ae1a7c57567
MarianoVanStaden <106852546+MarianoVanStaden@users.noreply.github.com>
2022-07-28 01:56:28 -0300

    Update carrito.js

--- js/carrito.js
+++ js/carrito.js
@@ -97,7 +97,7 @@ if (carrito.length == 0) {
                 <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
             </svg></button></th>
                 <th class='detallesTabla'><img class='imgProdCart' src=${img} alt='foto del producto'><span class='nombreProd'>${nombre}</span></th>
-                <th>x${cantidad} Kg</th>
+                <th>x${cantidad} Un</th>
                 <th id="text_price_${id}">$${(cantidad * precio).toFixed(2)}</th>
             </tr>`;
     tbody.innerHTML += cart;
